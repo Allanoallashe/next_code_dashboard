399b8a2b8c4666ef6195c6e2081c1b90e78801df
Allan Lenny <owinoallan77@gmail.com>
2024-06-16 01:33:42 +0300

    added a favicon

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,21 @@
 /** @format */
 
-import Header from "@/app/dashboard/page";
+import Head from "next/head";
 import "./globals.css";
 
 export const metadata = {
 	title: "Dashboard",
 	description: "built by @AllanoallasheDev",
+	icons: {
+		icon: [
+			{
+				rel: "icon",
+				url: "./ui/assets/pic1.jpg",
+				sizes: "32x32",
+				typeof: "image/x-icon",
+			},
+		],
+	},
 };
 
 export default function RootLayout({ children }) {
